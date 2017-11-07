53d2b0a034f62573ec9ee8ad971d4e07a53e6bca
Light-Bearing <hilo@list.ru>
2017-11-07 13:12:54 +0300

    Создали реакцию на нажатие кнопки ESC(скрыли большую картинку)

--- book/front-end-dev-resources/book-solutions/Chapter 05/ottergram/scripts/main.js
+++ book/front-end-dev-resources/book-solutions/Chapter 05/ottergram/scripts/main.js
@@ -2,7 +2,7 @@ var DETAIL_IMAGE_SELECTOR = '[data-image-role="target"]';
 var DETAIL_TITLE_SELECTOR = '[data-image-role="title"]';
 var THUMBNAIL_LINK_SELECTOR = '[data-image-role="trigger"]';
 var HIDDEN_DETAIL_CLASS = 'hidden-detail';
-
+var ESC_KEY = 27;
 function setDetails(imageUrl, titleText) {
   'use strict';
 
@@ -53,6 +53,9 @@ function addKeyPressHandler(){
   document.body.addEventListener('keyup', function (event){
     event.preventDefault();
     console.log(event.keyCode);
+    if(event.keyCode === ESC_KEY){
+      hideDetails();
+    }
   });
 }
 
