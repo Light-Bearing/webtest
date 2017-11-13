4bc854187dba4f0ceca829be27c59f3197ceda1c
Light-Bearing <hilo@list.ru>
2017-11-13 17:24:41 +0300

    Закончили на 297 странице

---
+++ CoffeeRun/js/formhandler.js
@@ -0,0 +1,52 @@
+(function(window) {
+  'use strict';
+  var App = window.App || {};
+  var $ = window.jQuery;
+
+  function FormHandler(selector) {
+    if (!selector) {
+      throw new Error('No selector provided');
+    }
+    this.$formElement = $(selector);
+    if (this.$formElement.length === 0) {
+      throw new Error('Could not find element with selector: ' + selector);
+    }
+  }
+
+  FormHandler.prototype.addSubmitHandler = function(fn) {
+    console.log('Setting submit handler for form');
+    this.$formElement.on('submit', function(event) {
+      event.preventDefault();
+      var data = {};
+      $(this).serializeArray().forEach(function(item) {
+        data[item.name] = item.value;
+        console.log(item.name + ' is ' + item.value);
+      });
+      console.log(data);
+      fn(data);
+      this.reset();
+      this.elements[0].focus();
+    });
+    FormHandler.prototype.addInputHandler = function(fn) {
+      console.log('Settings input for form');
+      this.$formElement.on('input', '[name="emailAddress"]', function (event){
+        var emailAdress =event.target.value;
+        //console.log(fn(emailAdress));
+        var message = '';
+        if (fn(emailAdress)){
+
+          event.target.setCustomValidity('');
+        } else {
+          message = emailAdress + ' is not an autorized email adress!\nAutorized only @big.com'
+          event.target.setCustomValidity(message);
+        }
+
+
+
+      });
+    };
+  }
+
+  App.FormHandler = FormHandler;
+  window.App = App;
+})(window);
---
+++ CoffeeRun/js/main.js
@@ -0,0 +1,24 @@
+(function(window) {
+  'use strinc';
+  var FORM_SELECTOR = '[data-coffee-order="form"]';
+  var CHECKLIST_SELECTOR = '[data-coffee-order="checklist"]';
+  var App = window.App;
+  var Truck = App.Truck;
+  var DataStore = App.DataStore;
+  var FormHandler = App.FormHandler;
+  var Validation = App.Validation;
+  var CheckList = App.CheckList;
+  var myTruck = new Truck('"ЭАЛ"', new DataStore());
+  window.myTruck = myTruck;
+  var checkList = new CheckList(CHECKLIST_SELECTOR);
+  checkList.addClickHendler(myTruck.deliverOrder.bind(myTruck));
+  var formHandler = new FormHandler(FORM_SELECTOR);
+
+  formHandler.addSubmitHandler(
+    function(data) {
+      myTruck.createOrder.call(myTruck, data);
+      checkList.addRow.call(checkList, data);
+    });
+  //console.log(formHandler);
+  formHandler.addInputHandler(Validation.isCompanyEmail);
+})(window);
